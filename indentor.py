feea46d7e4e6dc1a9b658d9b120c50c0a3cbfd20
rishav92 <rajrishav.2016@gmail.com>
2016-10-30 11:23:50 +0530

    comment added

--- indentor.py
+++ indentor.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python2
+#opening the file
 with open("raw.txt") as input_file:
-    text = input_file.read()
+    text = input_file.read()#reading the file and storing it in text
 istack = []
 str_length = 0
 indent = 0
 current_string = ""
-text = text.replace("\n{", "{")
-for char in text:
-    if char == '\n':
-        print current_string
-        current_string = " " * indent
+text = text.replace("\n{", "{")#replacing the "\n{" with "{" in entire text
+for char in text:#reading character one by one from text
+    if char == '\n':   #if "/n" is encountered
+        print current_string #print current_string
+        current_string = " " * indent  #replacing current value of current_string with intent times space
         str_length = 0
     elif not (char == '{' or char == '}' or char == '[' or char == ']' or char == ','):
-        str_length += 1
-        current_string += char
-    elif char == '}' or char == ']':
-        print current_string
-        current_string = " " * indent
-        print current_string + char
-        indent = istack.pop()
-        current_string = " " * indent
-        str_length = 0
-    elif char == '{' or char == '[':
-        istack.append(indent)
+        str_length += 1      #if other than "{","}",'[',']',or ',' is encountered the add to current_string
+        current_string += char #appending char to current_string
+    elif char == '}' or char == ']':  # if closing parenthesis is encountered
+        print current_string          #print current_string
+        current_string = " " * indent  # add indent to current_string
+        print current_string + char     #print current_string and char
+        indent = istack.pop()          # indent = last element of istack
+        current_string = " " * indent   #replacing current value of current_string with intent times space
+    elif char == '{' or char == '[': # if opening parenthesis is encountered
+        istack.append(indent)        #adding indent to last of istack
         indent = str_length + istack[-1]
         print current_string + char
-        current_string = " " * indent
+        current_string = " " * indent #replacing current value of current_string with intent times space
         str_length = 0
-    elif char == ',':
-        print current_string + char
-        current_string = " " * indent
+    elif char == ',':  #if "," is encountered
+        print current_string + char #print current_string + char
+        current_string = " " * indent #replacing current value of current_string with intent times space
         str_length = 0
